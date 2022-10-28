a00d8dc9bda7eb3c0e68513ecf69a201915f48dc
Bruno Jacob <brunojacob_89@hotmail.com>
2022-10-28 14:25:49 -0300

    add eureka

---
+++ loja/src/main/java/br/com/alura/microservice/loja/controller/service/CompraService.java
@@ -0,0 +1,26 @@
+package br.com.alura.microservice.loja.controller.service;
+
+import br.com.alura.microservice.loja.controller.dto.CompraDTO;
+import br.com.alura.microservice.loja.controller.dto.InfoFornecedorDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class CompraService {
+
+    @Autowired
+    private RestTemplate cliente;
+
+    public void realizaCompra(CompraDTO compra) {
+
+       /* RestTemplate cliente = new RestTemplate();*/
+        ResponseEntity<InfoFornecedorDTO> exchange =
+                cliente.exchange("http://fornecedor/info/" + compra.getEndereco().getEstado(),
+                HttpMethod.GET, null, InfoFornecedorDTO.class);
+
+        System.out.println(exchange.getBody().getEndereco());
+    }
+}
