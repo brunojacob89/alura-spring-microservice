2a910934a8fbeda1a0db6c9e79eae30d3640bf2e
Bruno Jacob <brunojacob_89@hotmail.com>
2022-11-04 03:27:18 -0300

    balanced

--- loja/src/main/java/br/com/alura/microservice/loja/controller/service/CompraService.java
+++ loja/src/main/java/br/com/alura/microservice/loja/controller/service/CompraService.java
@@ -1,26 +1,42 @@
 package br.com.alura.microservice.loja.controller.service;
 
+import br.com.alura.microservice.loja.client.FornecedorClient;
 import br.com.alura.microservice.loja.controller.dto.CompraDTO;
 import br.com.alura.microservice.loja.controller.dto.InfoFornecedorDTO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 @Service
 public class CompraService {
 
-    @Autowired
+   /* @Autowired
     private RestTemplate cliente;
+
+    @Autowired
+    private DiscoveryClient eurekaClient;*/
+
+    @Autowired
+    private FornecedorClient fornecedorClient;
 
     public void realizaCompra(CompraDTO compra) {
 
-       /* RestTemplate cliente = new RestTemplate();*/
-        ResponseEntity<InfoFornecedorDTO> exchange =
+        InfoFornecedorDTO info = fornecedorClient.getInfoPorEstado(compra.getEndereco().getEstado());
+        System.out.println(info.getEndereco());
+
+
+
+
+
+        /* RestTemplate cliente = new RestTemplate();*/
+     /*   ResponseEntity<InfoFornecedorDTO> exchange =
                 cliente.exchange("http://fornecedor/info/" + compra.getEndereco().getEstado(),
                 HttpMethod.GET, null, InfoFornecedorDTO.class);
 
-        System.out.println(exchange.getBody().getEndereco());
+        eurekaClient.getInstances("fornecedor").stream()
+                        .forEach(fornecedor -> {
+                            System.out.println("localhost:"+fornecedor.getPort());
+                        });
+
+        System.out.println(exchange.getBody().getEndereco());*/
     }
 }
